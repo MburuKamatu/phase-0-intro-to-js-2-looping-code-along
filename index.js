9be04e8fbbc8d6d2e26bb5d093ee03d12298670f
+ <+>
2022-09-09 12:16:16 +0300

    commit tree e5e62a7826d3306206bf4aeff8daf047a2757ea8

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+for (let countup = 0; countup < 10; countup++) {
+  console.log(countup);
+}
+
+function plantGarden() {
+  let keepWorking = true;
+  while (keepWorking) {
+    chooseSeedLocation();
+    plantSeed();
+    waterSeed();
+    keepWorking = checkForMoreSeeds();
+  }
+}
